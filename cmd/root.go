70e3749d93678a6e0093bf68e1c8205b3675f02d
Dev User <dev@example.com>
2026-05-16 09:40:00 +0000

    Remove redundant 'list' command in favor of 'brief' and 'scan'

--- cmd/root.go
+++ cmd/root.go
@@ -28,11 +28,10 @@ func init() {
 
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.lazycommit/config.toml)")
 
-	// Core Atomic Commands (5: commit, push, scan, list, brief)
+	// Core Atomic Commands (4: commit, push, scan, brief)
 	rootCmd.AddCommand(core.NewCommitCmd())
 	rootCmd.AddCommand(core.NewPushCmd())
 	rootCmd.AddCommand(core.NewScanCmd())
-	rootCmd.AddCommand(core.NewListCmd())
 	rootCmd.AddCommand(core.NewBriefCmd())
 
 	// Complex Dev Commands
